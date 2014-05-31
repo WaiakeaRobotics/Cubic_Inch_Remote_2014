2021e5d226d91906a1457f5f02b0fac0a912e5d2
Chester <hilo90mhz@gmail.com>
2014-05-30 15:55:28 -1000

    start

---
+++ Cubic_Inch_Robot_2014.ino
@@ -0,0 +1,46 @@
+#include <SPI.h>
+#include <nRF24L01p.h>
+
+nRF24L01p transmitter(14,8);//CSN,CE
+
+void setup(){
+  //delay(150);
+  Serial.begin(115200);
+  SPI.begin();
+  //SPI.setClockDivider(SPI_CLOCK_DIV2);
+  SPI.setBitOrder(MSBFIRST);
+  transmitter.channel(90);
+  transmitter.RXaddress("CIRem");
+  transmitter.TXaddress("CIBot");
+  transmitter.init();
+  Serial.println("I'm PTX as transceiver");
+}
+
+String message;
+
+ // the receive variable type must be the same as the type being received
+int PRXsays; 
+//String PRXsays;
+
+void loop(){
+  if(Serial.available()>0){
+    char incomming=Serial.read();
+    if(incomming=='\n'){
+      transmitter.txPL(message);
+      transmitter.send(SLOW);
+      message="";
+    }
+    else{
+      message+=incomming;
+    }
+  }
+  if(transmitter.available()){
+    transmitter.read();
+    transmitter.rxPL(PRXsays);
+    Serial.print("PRX says: \"");
+    Serial.print(PRXsays);
+    Serial.println("\"");
+    //PRXsays="";
+  }
+}
+
